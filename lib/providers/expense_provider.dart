885bfd13150f02f4257b5454e51793979f14165c
abhishekib <abhishekk@ibarts.in>
2025-04-01 12:47:37 +0530

    Refactor ExpenseProvider to integrate ExpenseService for asynchronous expense management

--- lib/providers/expense_provider.dart
+++ lib/providers/expense_provider.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/services/expense_service.dart';
 
 class ExpenseProvider with ChangeNotifier {
+  final ExpenseService _expenseService = ExpenseService();
   List<Expense> _expenses = [];
 
   List<Expense> get expenses => _expenses;
 
-  void addExpense(Expense newExpense) {
-    _expenses.add(newExpense);
-    notifyListeners();
+  ExpenseProvider() {
+    _loadExpenses();
   }
 
-  void updateExpense(String id, Expense updatedExpense) {
-    final index = _expenses.indexWhere((expense) => expense.id == id);
-    if (index != -1) {
-      _expenses[index] = updatedExpense;
+  void _loadExpenses() {
+    _expenseService.expensesStream.listen((expenses) {
+      _expenses = expenses;
       notifyListeners();
-    }
+    });
   }
 
-  void deleteExpense(String id) {
-    _expenses.removeWhere((expense) => expense.id == id);
-    notifyListeners();
+  Future<void> addExpense(Expense newExpense) async {
+    await _expenseService.addExpense(newExpense);
+  }
+
+  Future<void> updateExpense(String id, Expense updatedExpense) async {
+    await _expenseService.updateExpense(updatedExpense);
+  }
+
+  Future<void> deleteExpense(String id) async {
+    await _expenseService.deleteExpense(id);
   }
 }
